4cab5933fb92832249159cf290891ff809d7c1e6
Daniel A. Cifuentes C <dacifuentes@outlook.com>
2023-04-18 20:34:59 -0500

    :construction: Vercel config file added
    api directories adjusted!

--- api/index.js
+++ api/index.js
@@ -7,13 +7,14 @@ const {
 } = require('./middlewares/error.handler');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+const FRONTEND_URL = process.env.FRONTEND_URL || `http://localhost:${PORT}`;
 
 app.use(express.json());
-const whiteListCORS = ['http://localhost:8080', 'https://myapp.co'];
+const whiteListCORS = ['http://localhost:8080', FRONTEND_URL];
 const options = {
   origin: (origin, callback) => {
-    if (whiteListCORS.includes(origin)) {
+    if (whiteListCORS.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed'));
@@ -22,7 +23,7 @@ const options = {
 };
 app.use(cors(options));
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Prueba express');
 });
 
@@ -33,6 +34,6 @@ app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`App running on ${port}`);
+app.listen(PORT, () => {
+  console.log(`App running on ${PORT}`);
 });
