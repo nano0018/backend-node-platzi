08ae00a2626b78bc2f98be57f8857e1c4bbf2e25
Daniel A. Cifuentes C <dacifuentes@outlook.com>
2023-04-26 22:24:29 -0500

    :construction: Updated .gitignore
    Updated migrations and migrations configs

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -18,6 +18,7 @@ let URI = `${DB_ENGINE}://${USER}:${PASSWORD}@${config.dbHost}:${PORT}/${config.
 
 if (config.isProd) {
   URI = `${config.dbURL}`;
+  console.log(URI)
 }
 
 const sequelize = new Sequelize(URI, {
